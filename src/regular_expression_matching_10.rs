a0c119610f01066b31ff768931513ee9bb3b2333
Tony Duan <tony84727@gmail.com>
2021-07-15 22:02:39 +0800

    wip

--- src/regular_expression_matching_10.rs
+++ src/regular_expression_matching_10.rs
@@ -1,4 +1,9 @@
-use std::{ascii::EscapeDefault, collections::HashMap, os::macos::raw::stat, vec};
+use std::{
+    ascii::EscapeDefault,
+    collections::{HashMap, HashSet},
+    os::macos::raw::stat,
+    vec,
+};
 
 pub struct Solution;
 
@@ -23,54 +28,73 @@ impl NFA {
         let mut last_state = 0;
         if pattern.len() <= 0 {
             let mut transaction = HashMap::new();
-            transaction_table_map.insert(0, v)
-            transaction_table_list
-                .entry(Label::Epsilon)
-                .or_insert(vec![])
-                .push(0);
-            return Self {
-                transaction_table_map: transaction_table_list,
-                accept_state: 0,
-            };
+            transaction_table_map.insert(0, transaction);
+        } else {
+            for c in pattern.chars() {
+                match c {
+                    '*' => {
+                        transaction_table_map
+                            .entry(last_state)
+                            .or_insert(HashMap::new())
+                            .entry(Label::Epsilon)
+                            .or_insert(vec![])
+                            .push(last_state);
+                    }
+                    '.' => {
+                        let self_state = last_state;
+                        last_state += 1;
+                        transaction_table_map
+                            .entry(self_state)
+                            .or_insert(HashMap::new())
+                            .entry(Label::Epsilon)
+                            .or_insert(vec![])
+                            .push(last_state);
+                    }
+                    _ => {
+                        let self_state = last_state;
+                        last_state += 1;
+                        transaction_table_map
+                            .entry(self_state)
+                            .or_insert(HashMap::new())
+                            .entry(Label::Alphabet(c))
+                            .or_insert(vec![])
+                            .push(last_state);
+                    }
+                };
+            }
         }
-        for c in pattern.chars() {
-            match c {
-                '*' => {
-                    transaction_table_list
-                        .entry(Label::Epsilon)
-                        .or_insert(vec![])
-                        .push(last_state);
-                }
-                '.' => {
-                    last_state += 1;
-                    transaction_table_list
-                        .entry(Label::Epsilon)
-                        .or_insert(vec![])
-                        .push(last_state);
-                }
-                _ => {
-                    last_state += 1;
-                    transaction_table_list
-                        .entry(Label::Alphabet(c))
-                        .or_insert(vec![])
-                        .push(last_state);
-                }
-            };
-        }
+
         return Self {
-            transaction_table_map: transaction_table_list,
+            transaction_table_map: transaction_table_map,
             accept_state: last_state,
         };
     }
 
     fn closure(&self, current_state: u32, alphabet: char) -> Vec<u32> {
         let mut state_list = vec![];
-        if let Some(epsilon) = self.transaction_table_map.get(&Label::Epsilon) {
+        let transaction = self.transaction_table_map.get(&current_state).unwrap();
+        if let Some(epsilon) = transaction.get(&Label::Epsilon) {
             for next in epsilon.iter() {
                 state_list.push(*next);
             }
         }
+        if let Some(next_state_list) = transaction.get(&Label::Alphabet(alphabet)) {
+            for next in next_state_list.iter() {
+                state_list.push(*next);
+            }
+        }
         state_list
+    }
+}
+
+struct DFA {
+    transactions: HashMap<char, u32>,
+    acceptable_state_list: HashSet<u32>,
+}
+
+impl From<NFA> for DFA {
+    fn from(nfa: NFA) -> Self {
+        let mut stack =
     }
 }
 
