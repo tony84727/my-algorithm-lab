62e98f41d8d9f8618d2374ad0939b01ef55c01c8
Tony Duan <tony84727@gmail.com>
2021-07-18 12:40:44 +0800

    refactor: make solution modules public
    to prevent dead code warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-mod add_two_numbers_2;
+pub mod add_two_numbers_2;
 mod common;
-mod longest_palindromic_substring_5;
-mod merge_two_sorted_lists_21;
+pub mod longest_palindromic_substring_5;
+pub mod merge_two_sorted_lists_21;
