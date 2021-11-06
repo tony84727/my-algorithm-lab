7699f6909a7d0f1789559939bc7b698e31c7c824
Tony Duan <tony84727@gmail.com>
2021-11-06 21:35:19 +0800

    fix: test the right module

--- src/leetcode/single_number_136/mod.rs
+++ src/leetcode/single_number_136/mod.rs
@@ -15,6 +15,6 @@ mod tests {
     #[test_case(vec![2,2,1] => 1; "example 1")]
     #[test_case(vec![4,1,2,1,2] => 4; "example 2")]
     fn test_byxor_solution(nums: Vec<i32>) -> i32 {
-        super::bysort::Solution::single_number(nums)
+        super::byxor::Solution::single_number(nums)
     }
 }
