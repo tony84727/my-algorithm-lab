53ab70a7b3530b2ae3a79027283e91ade05a8572
Tony Duan <tony84727@gmail.com>
2021-11-09 21:33:09 +0800

    feat: add leetcode/1178 bitmask solution

--- src/leetcode/number_of_valid_words_for_each_puzzle_1178/mod.rs
+++ src/leetcode/number_of_valid_words_for_each_puzzle_1178/mod.rs
@@ -1,3 +1,4 @@
+pub mod bitmask;
 pub mod brute_force;
 
 #[cfg(test)]
@@ -11,5 +12,13 @@ mod tests {
         let words = words.iter().map(|s| s.to_string()).collect();
         let puzzles = puzzles.iter().map(|s| s.to_string()).collect();
         brute_force::Solution::find_num_of_valid_words(words, puzzles)
+    }
+
+    #[test_case(&["aaaa","asas","able","ability","actt","actor","access"], &["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"] => vec![1,1,3,2,4,0]; "example 1")]
+    #[test_case(&["apple","pleas","please"], &["aelwxyz","aelpxyz","aelpsxy","saelpxy","xaelpsy"] => vec![0,1,3,2,0]; "case 1")]
+    fn test_bitmap_solution(words: &[&str], puzzles: &[&str]) -> Vec<i32> {
+        let words = words.iter().map(|s| s.to_string()).collect();
+        let puzzles = puzzles.iter().map(|s| s.to_string()).collect();
+        bitmask::Solution::find_num_of_valid_words(words, puzzles)
     }
 }
