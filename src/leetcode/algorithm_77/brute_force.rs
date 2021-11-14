2968e2741dede33ca46974606e53cdc988c48b28
Tony Duan <tony84727@gmail.com>
2021-11-14 22:07:50 +0800

    wip

---
+++ src/leetcode/algorithm_77/brute_force.rs
@@ -0,0 +1,7 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn combine(n: i32, k: i32) -> Vec<Vec<i32>> {
+        unimplemented!()
+    }
+}
---
+++ src/leetcode/algorithm_77/mod.rs
@@ -0,0 +1,12 @@
+pub mod brute_force;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(4,2 => vec![vec![2,4],vec![3,4], vec![2,3], vec![1,2], vec![1,3], vec![1,4]]; "example 1")]
+    fn test_brute_force_solution(n: i32, length: i32) -> Vec<Vec<i32>> {
+        brute_force::Solution::combine(n, lengh)
+    }
+}
