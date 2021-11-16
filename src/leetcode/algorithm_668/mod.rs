7b4e3f33299f926c698b7fdee8bf7d6bf85ba205
Tony Duan <tony84727@gmail.com>
2021-11-16 12:28:25 +0800

    feat: leetcode/668 add brute force

---
+++ src/leetcode/algorithm_668/mod.rs
@@ -0,0 +1,13 @@
+pub mod brute_force;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(3,3,5 => 3; "example 1")]
+    #[test_case(2,3,6 => 6; "example 2")]
+    fn test_brute_force_solution(m: i32, n: i32, k: i32) -> i32 {
+        brute_force::Solution::find_kth_number(m, n, k)
+    }
+}
