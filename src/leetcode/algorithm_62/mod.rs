48e6c433653b384faa8aeb18d88ebe2141b876ed
Tony Duan <tony84727@gmail.com>
2021-11-17 22:16:46 +0800

    feat: add table memoized solution

--- src/leetcode/algorithm_62/mod.rs
+++ src/leetcode/algorithm_62/mod.rs
@@ -1,4 +1,5 @@
 pub mod recursive;
+pub mod table_memoized;
 
 #[cfg(test)]
 mod tests {
@@ -11,5 +12,13 @@ mod tests {
     #[test_case(3,3 => 6; "example 4")]
     fn test_brute_force(m: i32, n: i32) -> i32 {
         recursive::Solution::unique_paths(m, n)
+    }
+
+    #[test_case(3,7 => 28; "example 1")]
+    #[test_case(3,2 => 3; "example 2")]
+    #[test_case(7,3 => 28; "example 3")]
+    #[test_case(3,3 => 6; "example 4")]
+    fn test_table_memoized(m: i32, n: i32) -> i32 {
+        table_memoized::Solution::unique_paths(m, n)
     }
 }
---
+++ src/leetcode/algorithm_62/table_memoized.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn unique_paths(m: i32, n: i32) -> i32 {
+        let mut table = HashMap::new();
+        fn solve(table: &mut HashMap<(i32, i32), i32>, m: i32, n: i32) -> i32 {
+            match table.get(&(m, n)) {
+                Some(answer) => *answer,
+                None => {
+                    if m == 1 || n == 1 {
+                        table.insert((m, n), 1);
+                        return 1;
+                    }
+                    let answer = solve(table, m - 1, n) + solve(table, m, n - 1);
+                    table.insert((m, n), answer);
+                    answer
+                }
+            }
+        }
+        return solve(&mut table, m, n);
+    }
+}
