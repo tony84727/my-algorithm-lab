e46fc4f9c710e967f89b8a09116fe7ca287eda71
Tony Duan <tony84727@gmail.com>
2022-01-02 21:29:18 +0800

    perf: use unstable sort

--- src/leetcode/algorithm_49/sort.rs
+++ src/leetcode/algorithm_49/sort.rs
@@ -7,7 +7,7 @@ impl Solution {
         let mut buckets: HashMap<Vec<char>, Vec<String>> = HashMap::new();
         strs.into_iter().for_each(|anagram| {
             let mut key = anagram.chars().collect::<Vec<char>>();
-            key.sort();
+            key.sort_unstable();
             buckets.entry(key).or_default().push(anagram);
         });
         buckets.into_iter().map(|(_, value)| value).collect()
