18f147132ffa4e478053f0be984081c6f599518c
Tony Duan <tony84727@gmail.com>
2022-01-02 14:56:12 +0800

    feat: add leetcode/49 sort solution

--- src/leetcode/algorithm_49/mod.rs
+++ src/leetcode/algorithm_49/mod.rs
@@ -1,4 +1,5 @@
 pub mod custom_hashing;
+pub mod sort;
 
 #[cfg(test)]
 mod tests {
@@ -25,6 +26,17 @@ mod tests {
         assert_eq!(
             HashSet::<Vec<String>>::from_iter(answer.into_iter()),
             HashSet::from_iter(custom_hashing::Solution::group_anagrams(input).into_iter())
+        );
+    }
+
+    #[test_case("example1.ron")]
+    #[test_case("case1.ron")]
+    fn test_sort_solution(str: &str) {
+        let TestCase { input, answer } =
+            from_file(PathBuf::new().join("src/leetcode/algorithm_49").join(str));
+        assert_eq!(
+            HashSet::<Vec<String>>::from_iter(answer.into_iter()),
+            HashSet::from_iter(sort::Solution::group_anagrams(input).into_iter())
         );
     }
 }
---
+++ src/leetcode/algorithm_49/sort.rs
@@ -0,0 +1,15 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        let mut buckets: HashMap<Vec<char>, Vec<String>> = HashMap::new();
+        strs.into_iter().for_each(|anagram| {
+            let mut key = anagram.chars().collect::<Vec<char>>();
+            key.sort();
+            buckets.entry(key).or_default().push(anagram);
+        });
+        buckets.into_iter().map(|(_, value)| value).collect()
+    }
+}
