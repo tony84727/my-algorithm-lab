62d9be1612d20e8e856783a66889406854fe5757
Tony Duan <tony84727@gmail.com>
2022-01-02 18:19:07 +0800

    feat: tried with primes

--- src/leetcode/algorithm_49/custom_hashing.rs
+++ src/leetcode/algorithm_49/custom_hashing.rs
@@ -1,14 +1,21 @@
 use std::collections::HashMap;
+use std::num::Wrapping;
 
 pub struct Solution;
 
 impl Solution {
     pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+        const PRIMES: [i32; 26] = [
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+            97, 101, 103,
+        ];
         fn hash_string(input: &str) -> i32 {
-            i32::rotate_left(
-                input.chars().fold(0, |carry, c| carry + (c as i32 - 96)),
-                input.len() as u32,
-            )
+            input
+                .chars()
+                .fold(Wrapping(1_i32), |carry, c| {
+                    carry * Wrapping(PRIMES[(c as usize - 97)])
+                })
+                .0
         }
         let mut map: HashMap<i32, Vec<String>> = HashMap::new();
         for s in strs {
