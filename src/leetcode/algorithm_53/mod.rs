acae90e3e4b77a53759b0c7d32688624e1ac416f
Tony Duan <tony84727@gmail.com>
2022-01-15 20:33:48 +0800

    feat: add recursive solution for leetcode/53

--- src/leetcode/algorithm_53/mod.rs
+++ src/leetcode/algorithm_53/mod.rs
@@ -1,5 +1,6 @@
 pub mod brute_force;
 pub mod kadane;
+pub mod recursive;
 
 #[cfg(test)]
 mod tests {
@@ -22,5 +23,14 @@ mod tests {
     #[test_case(vec![-1] => -1; "case 4")]
     fn test_kadane_solution(nums: Vec<i32>) -> i32 {
         kadane::Solution::max_sub_array(nums)
+    }
+
+    #[test_case(vec![-2,1,-3,4,-1,2,1,-5,4] => 6; "example 1")]
+    #[test_case(vec![5,4,-1,7,8] => 23; "case 1")]
+    #[test_case(vec![1,2,3,4,5,6] => 21; "case 2")]
+    #[test_case(vec![1,2,3,-4,5,6] => 13; "case 3")]
+    #[test_case(vec![-1] => -1; "case 4")]
+    fn test_recursive_solution(nums: Vec<i32>) -> i32 {
+        recursive::Solution::max_sub_array(nums)
     }
 }
