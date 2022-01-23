4737a0aa50d87e87c684e7c4a67111a703c55ee3
Tony Duan <tony84727@gmail.com>
2022-01-23 21:10:17 +0800

    refactor: simplify

--- src/leetcode/algorithm_55/mod.rs
+++ src/leetcode/algorithm_55/mod.rs
@@ -12,6 +12,7 @@ mod tests {
     #[test_case(vec![0] => true; "case 2")]
     #[test_case(vec![0,2,3] => false; "case 3")]
     #[test_case(vec![1,2,0,1] => true; "case 4")]
+    #[test_case(vec![5,9,3,2,1,0,2,3,3,1,0,0] => true; "case 5")]
     fn test_flood_solution(nums: Vec<i32>) -> bool {
         flood::Solution::can_jump(nums)
     }
@@ -22,8 +23,7 @@ mod tests {
     #[test_case(vec![0] => true; "case 2")]
     #[test_case(vec![0,2,3] => false; "case 3")]
     #[test_case(vec![1,2,0,1] => true; "case 4")]
-    #[test_case(vec![5,9,3,2,1,0,2,3,3,1,0,0] => true; "case 5"
-		)]
+    #[test_case(vec![5,9,3,2,1,0,2,3,3,1,0,0] => true; "case 5")]
     fn test_max_range_solution(nums: Vec<i32>) -> bool {
         max_range::Solution::can_jump(nums)
     }
