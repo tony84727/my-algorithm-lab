32fc3a97b3890ff5c62cee699cb982cc1626b2fe
Tony Duan <tony84727@gmail.com>
2022-01-30 21:11:27 +0800

    feat: optimize

--- src/leetcode/algorithm_56/mod.rs
+++ src/leetcode/algorithm_56/mod.rs
@@ -3,14 +3,14 @@ pub struct Solution;
 impl Solution {
     pub fn merge(mut intervals: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
         fn merging(intervals: &mut Vec<Vec<i32>>) {
-            intervals.sort_unstable_by_key(|item| item[0]);
+            intervals.sort_unstable();
             let mut current = 0;
             while current < intervals.len() - 1 {
                 if intervals[current][1] >= intervals[current + 1][0] {
                     intervals[current + 1][0] = intervals[current][0];
                     intervals[current + 1][1] =
                         intervals[current][1].max(intervals[current + 1][1]);
-                    intervals.remove(current);
+                    intervals.swap_remove(current);
                 }
                 current += 1;
             }
