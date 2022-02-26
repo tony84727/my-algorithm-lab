8ff4c05a389be3e12e6ee60b70c86ec35b8b676e
Tony Duan <tony84727@gmail.com>
2022-02-26 16:27:23 +0800

    feat: leetcode/73 inplace solution

--- src/leetcode/algorithm_73/mod.rs
+++ src/leetcode/algorithm_73/mod.rs
@@ -1,3 +1,4 @@
+pub mod inplace_flags;
 pub mod skip;
 
 #[cfg(test)]
@@ -7,8 +8,18 @@ mod tests {
     use test_case::test_case;
 
     #[test_case(vecvec![[0,0,0,5], [4,3,1,4], [0,1,1,4], [1,2,1,3], [0,0,1,1]],vecvec![[0,0,0,0], [0,0,0,4], [0,0,0,0], [0,0,0,3], [0,0,0,0]]; "case 1")]
+    #[test_case(vecvec![[0,1,2,0], [3,4,5,2], [1,3,1,5]], vecvec![[0,0,0,0], [0,4,5,0], [0,3,1,0]]; "case 2")]
+    #[test_case(vecvec![[1,2,3,4], [5,0,7,8], [0,10,11,12], [13,14,15,0]], vecvec![[0,0,3,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]; "case 3")]
     fn test_skip_solution(mut input: Vec<Vec<i32>>, expected: Vec<Vec<i32>>) {
         skip::Solution::set_zeroes(&mut input);
+        assert_eq!(expected, input);
+    }
+
+    #[test_case(vecvec![[0,0,0,5], [4,3,1,4], [0,1,1,4], [1,2,1,3], [0,0,1,1]],vecvec![[0,0,0,0], [0,0,0,4], [0,0,0,0], [0,0,0,3], [0,0,0,0]]; "case 1")]
+    #[test_case(vecvec![[0,1,2,0], [3,4,5,2], [1,3,1,5]], vecvec![[0,0,0,0], [0,4,5,0], [0,3,1,0]]; "case 2")]
+    #[test_case(vecvec![[1,2,3,4], [5,0,7,8], [0,10,11,12], [13,14,15,0]], vecvec![[0,0,3,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]; "case 3")]
+    fn test_inplace_flags_solution(mut input: Vec<Vec<i32>>, expected: Vec<Vec<i32>>) {
+        inplace_flags::Solution::set_zeroes(&mut input);
         assert_eq!(expected, input);
     }
 }
