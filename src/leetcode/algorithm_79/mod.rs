4308d042e4763797c607e05b481cb7484929fd86
Tony Duan <tony84727@gmail.com>
2022-02-27 21:27:49 +0800

    test: leetcode/79 case 2

--- src/leetcode/algorithm_79/mod.rs
+++ src/leetcode/algorithm_79/mod.rs
@@ -7,6 +7,7 @@ mod tests {
 
     #[test_case(vec!["ABCE", "SFCS", "ADEE"], "ABCCED" => true; "example 1")]
     #[test_case(vec!["ABCE", "SFCS", "ADEE"], "ABCB" => false; "case 1")]
+    #[test_case(vec!["ABCE", "SFES", "ADEE"], "ABCEFSADEESE" => true; "case 2")]
     fn test_recursive_solution(board: Vec<&str>, word: &str) -> bool {
         let board = board.into_iter().map(|s| s.chars().collect()).collect();
         recursive::Solution::exist(board, word.to_string())
