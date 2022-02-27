66a43e0ad0e7eacda0d36b3423e44bc2ae20f61c
Tony Duan <tony84727@gmail.com>
2022-02-27 11:59:04 +0800

    feat(leetcode/79): prevent revisit

--- src/leetcode/algorithm_79/mod.rs
+++ src/leetcode/algorithm_79/mod.rs
@@ -6,6 +6,7 @@ mod tests {
     use test_case::test_case;
 
     #[test_case(vec!["ABCE", "SFCS", "ADEE"], "ABCCED" => true; "example 1")]
+    #[test_case(vec!["ABCE", "SFCS", "ADEE"], "ABCB" => false; "case 1")]
     fn test_recursive_solution(board: Vec<&str>, word: &str) -> bool {
         let board = board.into_iter().map(|s| s.chars().collect()).collect();
         recursive::Solution::exist(board, word.to_string())
--- src/leetcode/algorithm_79/recursive.rs
+++ src/leetcode/algorithm_79/recursive.rs
@@ -8,40 +8,79 @@ impl Solution {
             size: (usize, usize),
             from: (usize, usize),
             target: &str,
+            mut visited: Vec<Vec<bool>>,
         ) -> bool {
             if target.is_empty() {
-                return false;
+                return true;
             }
+            visited[from.0][from.1] = true;
             let first = target.chars().nth(0).unwrap();
             // top
-            if from.0 > 0 && board[from.0 - 1][from.1] == first {
-                if find(board, size, (from.0 - 1, from.1), &target[1..]) {
-                    return true;
+            if from.0 > 0 {
+                let next = (from.0 - 1, from.1);
+                let &is_visited = visited
+                    .get(next.0)
+                    .and_then(|row| row.get(next.1))
+                    .unwrap_or(&false);
+                if !is_visited && board[next.0][next.1] == first {
+                    if find(board, size, next, &target[1..], visited.clone()) {
+                        return true;
+                    }
                 }
             }
             // bottom
-            if from.0 < size.0 - 1 && board[from.0 + 1][from.1] == first {
-                if find(board, size, (from.0 + 1, from.1), &target[1..]) {
-                    return true;
+            if from.0 < size.0 - 1 {
+                let next = (from.0 + 1, from.1);
+                let &is_visited = visited
+                    .get(next.0)
+                    .and_then(|row| row.get(next.1))
+                    .unwrap_or(&false);
+                if !is_visited && board[next.0][next.1] == first {
+                    if find(board, size, next, &target[1..], visited.clone()) {
+                        return true;
+                    }
                 }
             }
             // left
-            if from.1 > 0 && board[from.0][from.1 - 1] == first {
-                if find(board, size, (from.0, from.1 - 1), &target[1..]) {
-                    return true;
+            if from.1 > 0 {
+                let next = (from.0, from.1 - 1);
+                let &is_visited = visited
+                    .get(next.0)
+                    .and_then(|row| row.get(next.1))
+                    .unwrap_or(&false);
+                if !is_visited && board[next.0][next.1] == first {
+                    if find(board, size, next, &target[1..], visited.clone()) {
+                        return true;
+                    }
                 }
             }
             // right
-            if from.1 < size.1 - 1 && board[from.0][from.1 + 1] == first {
-                if find(board, size, (from.0, from.1 + 1), &target[1..]) {
-                    return true;
+            if from.1 < size.1 - 1 {
+                let next = (from.0, from.1 + 1);
+                let &is_visited = visited
+                    .get(next.0)
+                    .and_then(|row| row.get(next.1))
+                    .unwrap_or(&false);
+                if !is_visited && board[next.0][next.1] == first {
+                    if find(board, size, next, &target[1..], visited.clone()) {
+                        return true;
+                    }
                 }
             }
             false
         }
-        for w in 0..size.0 {
-            for h in 0..size.1 {
-                if find(&board, size, (w, h), &word) {
+        for h in 0..size.0 {
+            for w in 0..size.1 {
+                let begin = word.chars().nth(0).unwrap();
+                if board[h][w] == begin
+                    && find(
+                        &board,
+                        size,
+                        (h, w),
+                        &word[1..],
+                        vec![vec![false; size.1]; size.0],
+                    )
+                {
                     return true;
                 }
             }
