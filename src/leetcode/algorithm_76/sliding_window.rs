04ada7b4c4e51c2374518a8d1379a9f173fd5500
Tony Duan <tony84727@gmail.com>
2022-03-05 18:57:41 +0800

    perf(leetcode/76): optimize

--- src/leetcode/algorithm_76/sliding_window.rs
+++ src/leetcode/algorithm_76/sliding_window.rs
@@ -1,71 +1,76 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct Solution;
 
-impl Solution {
-    pub fn min_window(s: String, t: String) -> String {
-        fn satisfy(to_find: &HashMap<char, i32>) -> bool {
-            for (_, &v) in to_find.iter() {
-                if v > 0 {
-                    return false;
+struct Scanning {
+    to_find: HashMap<char, i32>,
+    missing: HashSet<char>,
+}
+
+impl Scanning {
+    fn new(target: String) -> Self {
+        let mut to_find = HashMap::<char, i32>::new();
+        let mut missing = HashSet::new();
+        for c in target.chars() {
+            *to_find.entry(c).or_default() += 1;
+            missing.insert(c);
+        }
+        Self { to_find, missing }
+    }
+    fn meet(&mut self, c: char) {
+        match self.to_find.get_mut(&c) {
+            Some(count) => {
+                *count -= 1;
+                if *count <= 0 {
+                    self.missing.remove(&c);
                 }
             }
-            true
+            None => (),
         }
+    }
+
+    fn remove(&mut self, c: char) {
+        match self.to_find.get_mut(&c) {
+            Some(count) => {
+                *count += 1;
+                if *count > 0 {
+                    self.missing.insert(c);
+                }
+            }
+            None => (),
+        }
+    }
+
+    fn is_satisfied(&self) -> bool {
+        return self.missing.is_empty();
+    }
+}
+
+impl Solution {
+    pub fn min_window(s: String, t: String) -> String {
+        let mut scanning = Scanning::new(t);
         let mut right = 0;
         let mut answer: Option<&[char]> = None;
         let s: Vec<char> = s.chars().collect();
-        let mut to_find = {
-            let mut target = HashMap::<char, i32>::new();
-            for c in t.chars() {
-                *target.entry(c).or_default() += 1;
-            }
-            target
-        };
-
-        let mut is_satisfied = false;
-
         for (left, first) in s.iter().enumerate() {
-            while !is_satisfied && right < s.len() {
+            while !scanning.is_satisfied() && right < s.len() {
                 // try to shrink by moving left
                 right += 1;
-                if let Some(count) = to_find.get_mut(&s[right - 1]) {
-                    *count -= 1;
-                    if satisfy(&to_find) {
-                        is_satisfied = true;
-                        match answer {
-                            Some(current) if right - left < current.len() => {
-                                answer = Some(&s[left..right]);
-                            }
-                            None => {
-                                answer = Some(&s[left..right]);
-                            }
-                            _ => (),
-                        }
-                    }
-                }
+                scanning.meet(s[right - 1]);
             }
-            if is_satisfied {
-                match to_find.get_mut(first) {
-                    Some(count) => {
-                        *count += 1;
-                        if *count > 0 {
-                            match answer {
-                                Some(current) if right - left < current.len() => {
-                                    answer = Some(&s[left..right]);
-                                }
-                                None => {
-                                    answer = Some(&s[left..right]);
-                                }
-                                _ => (),
-                            }
-                            if right >= s.len() {
-                                break;
-                            }
-                            is_satisfied = false;
-                        }
+            scanning.remove(*first);
+            if !scanning.is_satisfied() {
+                match answer {
+                    Some(current) if right - left < current.len() => {
+                        answer = Some(&s[left..right]);
                     }
-                    None => (),
+                    None => {
+                        answer = Some(&s[left..right]);
+                    }
+                    _ => (),
+                }
+                if right >= s.len() {
+                    break;
                 }
             }
         }
