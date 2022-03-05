3e313ddbb78bdf1eb74d43d6c59a606f2de8e61f
Tony Duan <tony84727@gmail.com>
2022-03-05 14:33:18 +0800

    perf(leetcode/76): optimize

--- src/leetcode/algorithm_76/sliding_window.rs
+++ src/leetcode/algorithm_76/sliding_window.rs
@@ -4,16 +4,10 @@ pub struct Solution;
 
 impl Solution {
     pub fn min_window(s: String, t: String) -> String {
-        fn satisfy(target: &HashMap<char, usize>, current: &HashMap<char, usize>) -> bool {
-            for (k, v) in target.iter() {
-                match current.get(k) {
-                    None => {
-                        return false;
-                    }
-                    Some(current_count) if current_count < v => {
-                        return false;
-                    }
-                    _ => (),
+        fn satisfy(to_find: &HashMap<char, i32>) -> bool {
+            for (_, &v) in to_find.iter() {
+                if v > 0 {
+                    return false;
                 }
             }
             true
@@ -21,25 +15,23 @@ impl Solution {
         let mut right = 0;
         let mut answer: Option<&[char]> = None;
         let s: Vec<char> = s.chars().collect();
-        let t = {
-            let mut target = HashMap::<char, usize>::new();
+        let mut to_find = {
+            let mut target = HashMap::<char, i32>::new();
             for c in t.chars() {
                 *target.entry(c).or_default() += 1;
             }
             target
         };
 
-        let mut meet = HashMap::<char, usize>::new();
         let mut is_satisfied = false;
 
         for (left, first) in s.iter().enumerate() {
             while !is_satisfied && right < s.len() {
                 // try to shrink by moving left
                 right += 1;
-                if t.contains_key(&s[right - 1]) {
-                    // meet.insert(s[right]);
-                    *meet.entry(s[right - 1]).or_default() += 1;
-                    if satisfy(&t, &meet) {
+                if let Some(count) = to_find.get_mut(&s[right - 1]) {
+                    *count -= 1;
+                    if satisfy(&to_find) {
                         is_satisfied = true;
                         match answer {
                             Some(current) if right - left < current.len() => {
@@ -54,11 +46,10 @@ impl Solution {
                 }
             }
             if is_satisfied {
-                match t.get(first) {
-                    Some(&demand) => {
-                        let count = meet.get_mut(first).unwrap();
-                        *count -= 1;
-                        if *count < demand {
+                match to_find.get_mut(first) {
+                    Some(count) => {
+                        *count += 1;
+                        if *count > 0 {
                             match answer {
                                 Some(current) if right - left < current.len() => {
                                     answer = Some(&s[left..right]);
