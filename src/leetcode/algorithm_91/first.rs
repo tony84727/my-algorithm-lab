3257987197d4bb04e7df0b9881dc0f82f87f76bd
Tony Duan <tony84727@gmail.com>
2022-03-13 12:06:56 +0800

    perf: optimize

--- src/leetcode/algorithm_91/first.rs
+++ src/leetcode/algorithm_91/first.rs
@@ -5,33 +5,29 @@ impl Solution {
         if s.len() == 0 {
             return 0;
         }
-        fn char_to_digit(c: char) -> u8 {
-            ((c as u8) - 0x30) as u8
-        }
-        let digits: Vec<u8> = s.chars().map(char_to_digit).collect();
-        if digits[0] == 0 {
+        let mut previous = 1;
+        let mut ways = 1;
+        let mut digits = s.chars().map(|c| c as u8 - 0x30);
+        let mut last = digits.next().unwrap();
+        if last == 0 {
             return 0;
         }
-        let mut ways = vec![1, 1, 1];
-
-        for (i, &d) in digits.iter().enumerate().skip(1) {
-            let previous = digits[i - 1];
+        for d in digits {
             if d == 0 {
-                if previous == 1 || previous == 2 {
-                    ways.rotate_right(1);
-                    ways[0] = ways[2];
+                if last > 0 && last <= 2 {
+                    ways = previous;
                 } else {
                     return 0;
                 }
-                continue;
-            }
-            ways.rotate_right(1);
-            if (previous == 1 || previous == 2) && (previous * 10 + d <= 26) {
-                ways[0] = ways[1] + ways[2];
             } else {
-                ways[0] = ways[1];
+                let temp = ways;
+                if last > 0 && last * 10 + d <= 26 {
+                    ways += previous;
+                }
+                previous = temp;
             }
+            last = d;
         }
-        ways[0]
+        ways
     }
 }
