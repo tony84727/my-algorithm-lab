6cb7737dafcf59acd0f54e56eac31d79f18f3ff6
Tony Duan <tony84727@gmail.com>
2022-03-27 13:12:43 +0800

    refactor: extract TreeNode to common

--- src/leetcode/common.rs
+++ src/leetcode/common.rs
@@ -1,3 +1,4 @@
+use std::{cell::RefCell, rc::Rc};
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
     pub val: i32,
@@ -40,5 +41,23 @@ pub mod test_utils {
     pub fn from_file<T: DeserializeOwned, P: AsRef<Path>>(path: P) -> T {
         let mut f = File::open(path).unwrap();
         ron::de::from_reader(&mut f).unwrap()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
     }
 }
