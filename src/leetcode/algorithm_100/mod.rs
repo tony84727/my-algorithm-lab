6457bea1bc5bd3e6ed883f9fa6d3568601867e7d
Tony Duan <tony84727@gmail.com>
2022-04-03 12:24:10 +0800

    feat(leetcode/100): preorder dump

---
+++ src/leetcode/algorithm_100/mod.rs
@@ -0,0 +1,20 @@
+pub mod dump;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::leetcode::common::TreeNode;
+    use test_case::test_case;
+
+    #[test_case(vec!["1", "2", "3"], vec!["1", "2", "3"] => true; "example 1")]
+    #[test_case(vec!["1", "2"], vec!["1", "null", "2"] => false; "case 1")]
+    #[test_case(vec!["1", "2", "1"], vec!["1", "1", "2"] => false; "case 2")]
+    #[test_case(vec!["0"], vec!["1"] => false; "case 3")]
+    #[test_case(vec!["1", "1"], vec!["1", "null", "1"] => false; "case 4")]
+    fn test_dump_solution(p: Vec<&str>, q: Vec<&str>) -> bool {
+        let p = TreeNode::from_preorder_str(p);
+        let q = TreeNode::from_preorder_str(q);
+        dump::Solution::is_same_tree(p, q)
+    }
+}
