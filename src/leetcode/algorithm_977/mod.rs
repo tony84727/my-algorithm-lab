fe142a63eecc89b89218c74ad61af6133de81de5
Tony Duan <tony84727@gmail.com>
2022-04-05 15:50:00 +0800

    feat(leetcode/977): merging

--- src/leetcode/algorithm_977/mod.rs
+++ src/leetcode/algorithm_977/mod.rs
@@ -1,4 +1,5 @@
 pub mod brute;
+pub mod merging;
 
 #[cfg(test)]
 mod tests {
@@ -6,7 +7,13 @@ mod tests {
     use test_case::test_case;
 
     #[test_case(vec![-4,-1,0,3,10] => vec![0,1,9,16,100]; "example 1")]
-    fn test_solution(nums: Vec<i32>) -> Vec<i32> {
+    fn test_brute_solution(nums: Vec<i32>) -> Vec<i32> {
         brute::Solution::sorted_squares(nums)
+    }
+
+    #[test_case(vec![-4,-1,0,3,10] => vec![0,1,9,16,100]; "example 1")]
+    #[test_case(vec![1] => vec![1]; "case 1")]
+    fn test_merging_solution(nums: Vec<i32>) -> Vec<i32> {
+        merging::Solution::sorted_squares(nums)
     }
 }
