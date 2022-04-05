0d6c1a89dd4671490c9283b4ffbd884fcd834434
Tony Duan <tony84727@gmail.com>
2022-04-05 09:43:40 +0800

    feat(leetcode/100): recursive

--- src/leetcode/algorithm_100/mod.rs
+++ src/leetcode/algorithm_100/mod.rs
@@ -1,5 +1,6 @@
 pub mod compare;
 pub mod dump;
+pub mod recursive;
 
 #[cfg(test)]
 mod tests {
@@ -27,5 +28,16 @@ mod tests {
         let p = TreeNode::from_preorder_str(p);
         let q = TreeNode::from_preorder_str(q);
         compare::Solution::is_same_tree(p, q)
+    }
+
+    #[test_case(vec!["1", "2", "3"], vec!["1", "2", "3"] => true; "example 1")]
+    #[test_case(vec!["1", "2"], vec!["1", "null", "2"] => false; "case 1")]
+    #[test_case(vec!["1", "2", "1"], vec!["1", "1", "2"] => false; "case 2")]
+    #[test_case(vec!["0"], vec!["1"] => false; "case 3")]
+    #[test_case(vec!["1", "1"], vec!["1", "null", "1"] => false; "case 4")]
+    fn test_recursive_solution(p: Vec<&str>, q: Vec<&str>) -> bool {
+        let p = TreeNode::from_preorder_str(p);
+        let q = TreeNode::from_preorder_str(q);
+        recursive::Solution::is_same_tree(p, q)
     }
 }
