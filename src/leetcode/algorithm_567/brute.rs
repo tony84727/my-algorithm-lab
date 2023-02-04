6dcbaedded5f7a9439b440d7d59b001adfded3e5
Tony Duan <tony84727@gmail.com>
2023-02-05 01:52:12 +0800

    feat(leetcode/567): brute force

---
+++ src/leetcode/algorithm_567/brute.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+pub struct Solution;
+
+impl Solution {
+    pub fn check_inclusion(s1: String, s2: String) -> bool {
+        let target = {
+            let mut m = HashMap::<char, i32>::new();
+            for c in s1.chars() {
+                *m.entry(c).or_default() += 1;
+            }
+            m
+        };
+        'a: for (i, start) in s2.char_indices() {
+            let mut remaining = target.clone();
+            for end in s2.chars().skip(i) {
+                match remaining.get_mut(&end) {
+                    Some(count) => {
+                        *count -= 1;
+                    }
+                    None => continue 'a,
+                }
+                if remaining.values().all(|&x| x == 0) {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
--- src/leetcode/algorithm_567/mod.rs
+++ src/leetcode/algorithm_567/mod.rs
@@ -1,1 +1,14 @@
+pub mod brute;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case("ab", "eidbaooo" => true; "example 1")]
+    #[test_case("ab", "eidboaoo" => false; "example 2")]
+    #[test_case("adc", "dcda" => true; "case 1")]
+    fn test_solution(s1: &str, s2: &str) -> bool {
+        brute::Solution::check_inclusion(s1.to_owned(), s2.to_owned())
+    }
+}
