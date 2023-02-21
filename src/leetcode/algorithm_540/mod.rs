1044513d4a8950dd1630fa3017a9c8282b78a348
Tony Duan <tony84727@gmail.com>
2023-02-21 20:42:37 +0800

    refactor: leetcode/540

--- src/leetcode/algorithm_540/mod.rs
+++ src/leetcode/algorithm_540/mod.rs
@@ -1,18 +1,12 @@
-pub struct Solution;
-
-impl Solution {
-    pub fn single_non_duplicate(nums: Vec<i32>) -> i32 {
-        nums.into_iter().fold(0, |acc, c| acc ^ c)
-    }
-}
+pub mod xor;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::xor;
     use test_case::test_case;
 
     #[test_case(vec![1,1,2,3,3,4,4,8,8] => 2; "example 1")]
-    fn test_solution(nums: Vec<i32>) -> i32 {
-        Solution::single_non_duplicate(nums)
+    fn test_xor_solution(nums: Vec<i32>) -> i32 {
+        xor::Solution::single_non_duplicate(nums)
     }
 }
