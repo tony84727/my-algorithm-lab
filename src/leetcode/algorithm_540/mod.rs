3ee9a29bae74ee86a908b2f0eb8900a216703b24
Tony Duan <tony84727@gmail.com>
2023-02-22 20:58:10 +0800

    feat(leetcode/540): binary search

--- src/leetcode/algorithm_540/mod.rs
+++ src/leetcode/algorithm_540/mod.rs
@@ -1,12 +1,28 @@
+pub mod binary_search;
 pub mod xor;
 
 #[cfg(test)]
 mod tests {
-    use super::xor;
+    use super::*;
     use test_case::test_case;
 
     #[test_case(vec![1,1,2,3,3,4,4,8,8] => 2; "example 1")]
+    #[test_case(vec![3,3,7,7,10,11,11] => 10; "example 2")]
+    #[test_case(vec![1,1,2,2,3,4,4,8,8] => 3; "case 1")]
+    #[test_case(vec![1,1,2,2,3,3,4,8,8] => 4; "case 2")]
+    #[test_case(vec![1] => 1; "case 3")]
+    #[test_case(vec![1,1,2] => 2; "case 4")]
     fn test_xor_solution(nums: Vec<i32>) -> i32 {
         xor::Solution::single_non_duplicate(nums)
+    }
+
+    #[test_case(vec![1,1,2,3,3,4,4,8,8] => 2; "example 1")]
+    #[test_case(vec![3,3,7,7,10,11,11] => 10; "example 2")]
+    #[test_case(vec![1,1,2,2,3,4,4,8,8] => 3; "case 1")]
+    #[test_case(vec![1,1,2,2,3,3,4,8,8] => 4; "case 2")]
+    #[test_case(vec![1] => 1; "case 3")]
+    #[test_case(vec![1,1,2] => 2; "case 4")]
+    fn test_binary_search_solution(nums: Vec<i32>) -> i32 {
+        binary_search::Solution::single_non_duplicate(nums)
     }
 }
