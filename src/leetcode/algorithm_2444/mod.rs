c00b3370afa925588d2f81cb64af75b44cba4a48
Tony Duan <tony84727@gmail.com>
2023-03-04 19:49:57 +0800

    feat(leetcode/2444): semi brute-force

--- src/leetcode/algorithm_2444/mod.rs
+++ src/leetcode/algorithm_2444/mod.rs
@@ -1,35 +1,47 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
-        let mut min_count = 0;
-        let mut max_count = 0;
-        let mut right = 0;
+    fn subarray_combination(nums: &[i32], min_k: i32, max_k: i32) -> i64 {
         let mut count = 0;
-        for taken in nums.iter() {
-            while right < nums.len() && nums[right] >= min_k && nums[right] <= max_k {
-                if nums[right] == min_k {
-                    min_count += 1;
+        for left in 0..nums.len() {
+            let mut min = 0;
+            let mut max = 0;
+            for &add in nums.iter().skip(left) {
+                if add == min_k {
+                    min += 1;
                 }
-                if nums[right] == max_k {
-                    max_count += 1;
+                if add == max_k {
+                    max += 1;
                 }
-                if min_count > 0 && max_count > 0 {
+                if min > 0 && max > 0 {
                     count += 1;
                 }
-                right += 1;
-            }
-            if min_count > 0 && max_count > 0 {
-                count += 1;
-            }
-            if *taken == min_k {
-                min_count -= 1;
-            }
-            if *taken == max_k {
-                max_count -= 1;
             }
         }
         count
+    }
+    pub fn count_subarrays(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
+        let mut last = None;
+        let mut segments: Vec<&[i32]> = vec![];
+        for (i, &n) in nums.iter().enumerate() {
+            if n > max_k || n < min_k {
+                if let Some(last) = last {
+                    segments.push(&nums[last..i]);
+                }
+                last = Some(i + 1);
+            }
+            if last.is_none() {
+                last = Some(i);
+            }
+        }
+        segments.push(match last {
+            Some(last) => &nums[last..],
+            None => &nums[..],
+        });
+        segments
+            .into_iter()
+            .map(|segment| Self::subarray_combination(segment, min_k, max_k))
+            .sum()
     }
 }
 
@@ -38,7 +50,10 @@ mod tests {
     use super::*;
     use test_case::test_case;
 
+    #[test_case(vec![1,3,5,2,7,5], 1,5 => 2; "example 1")]
     #[test_case(vec![1,1,1,1],1,1 => 10; "example 2")]
+    #[test_case(vec![934372,927845,479424,49441,17167,17167,65553,927845,17167,927845,17167,425106,17167,927845,17167,927845,251338,17167], 17167, 927845 => 118; "case 1")]
+    #[test_case(vec![9,4,2], 2, 4 => 1; "case 2")]
     fn test_solution(nums: Vec<i32>, min_k: i32, max_k: i32) -> i64 {
         Solution::count_subarrays(nums, min_k, max_k)
     }
