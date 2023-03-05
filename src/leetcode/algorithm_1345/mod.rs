a62bc8572877c9ea23dfa405ad91fb6442f48a0b
Tony Duan <tony84727@gmail.com>
2023-03-05 12:41:57 +0800

    feat(leetcode/1345): bfs
    optimize: prevent revisiting

--- src/leetcode/algorithm_1345/mod.rs
+++ src/leetcode/algorithm_1345/mod.rs
@@ -4,22 +4,29 @@ pub struct Solution;
 
 impl Solution {
     fn reachable(
-        connected: &HashMap<i32, Vec<usize>>,
+        connected: &mut HashMap<i32, Vec<usize>>,
+        visited: &mut Vec<bool>,
         arr: &[i32],
         i: usize,
     ) -> Option<Vec<usize>> {
+        visited[i] = true;
         let target = arr.len() - 1;
         let mut reachable = vec![];
-        if i > 0 {
+        if i > 0 && !visited[i - 1] {
             reachable.push(i - 1);
         }
-        if i < arr.len() - 1 {
+        if i < arr.len() - 1 && !visited[i + 1] {
             reachable.push(i + 1);
             if i + 1 == target {
                 return None;
             }
         }
-        for &next in connected.get(&arr[i]).unwrap().iter().filter(|x| **x != i) {
+        for next in connected
+            .remove(&arr[i])
+            .unwrap_or_default()
+            .into_iter()
+            .filter(|x| !visited[*x])
+        {
             if next == target {
                 return None;
             }
@@ -33,6 +40,7 @@ impl Solution {
         }
         let mut step = 1;
         let mut to_visit = vec![0];
+        let mut visisted = vec![false; arr.len()];
         let mut connected = HashMap::<i32, Vec<usize>>::new();
         for (i, n) in arr.iter().enumerate() {
             connected.entry(*n).or_default().push(i);
@@ -40,7 +48,7 @@ impl Solution {
         'stepping: loop {
             let current = std::mem::replace(&mut to_visit, vec![]);
             for i in current.into_iter() {
-                let next_step = Self::reachable(&connected, &arr, i);
+                let next_step = Self::reachable(&mut connected, &mut visisted, &arr, i);
                 match next_step {
                     Some(mut next_step) => {
                         to_visit.append(&mut next_step);
