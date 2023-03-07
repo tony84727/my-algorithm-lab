3fda87cd0b8f5e3ee2590311a428183061cc0939
Tony Duan <tony84727@gmail.com>
2023-03-07 23:41:14 +0800

    feat(leetcode/2187): binary search

--- src/leetcode/algorithm_2187/mod.rs
+++ src/leetcode/algorithm_2187/mod.rs
@@ -1,3 +1,4 @@
+pub mod binary_search;
 pub mod brute;
 
 #[cfg(test)]
@@ -7,7 +8,13 @@ mod tests {
 
     #[test_case(vec![1,2,3], 5 => 3; "example 1")]
     #[test_case(vec![2], 1 => 2; "example 2")]
-    fn test_solution(time: Vec<i32>, total_trips: i32) -> i64 {
+    fn test_brute_solution(time: Vec<i32>, total_trips: i32) -> i64 {
         brute::Solution::minimum_time(time, total_trips)
+    }
+
+    #[test_case(vec![1,2,3], 5 => 3; "example 1")]
+    #[test_case(vec![2], 1 => 2; "example 2")]
+    fn test_binary_search_solution(time: Vec<i32>, total_trips: i32) -> i64 {
+        binary_search::Solution::minimum_time(time, total_trips)
     }
 }
