7ae74472c54afdba4ab628441d18f5a9655dd192
Tony Duan <tony84727@gmail.com>
2023-03-18 20:20:15 +0800

    feat(leetcode/1472): stack

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -13,6 +13,7 @@ pub mod algorithm_125;
 pub mod algorithm_1345;
 pub mod algorithm_1413;
 pub mod algorithm_1470;
+pub mod algorithm_1472;
 pub mod algorithm_152;
 pub mod algorithm_1523;
 pub mod algorithm_1539;
