2b077471cbba52fb3673548ddad5e05bee1dc2e1
Tony Duan <tony84727@gmail.com>
2023-03-26 15:08:15 +0800

    feat(leetcode/2360): dfs_and_stack
    still TLE

--- src/leetcode/algorithm_2360/mod.rs
+++ src/leetcode/algorithm_2360/mod.rs
@@ -1,3 +1,4 @@
+pub mod dfs_and_stack;
 pub mod multi_dfs;
 
 #[cfg(test)]
@@ -9,5 +10,11 @@ mod tests {
     #[test_case(vec![1,2,0,4,5,6,3,8,9,7] => 4; "case 1")]
     fn test_multi_dfs_solution(edges: Vec<i32>) -> i32 {
         multi_dfs::Solution::longest_cycle(edges)
+    }
+
+    #[test_case(vec![3,3,4,2,3] => 3; "example 1")]
+    #[test_case(vec![1,2,0,4,5,6,3,8,9,7] => 4; "case 1")]
+    fn test_dfs_with_stack_solution(edges: Vec<i32>) -> i32 {
+        dfs_and_stack::Solution::longest_cycle(edges)
     }
 }
