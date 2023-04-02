6fa0342ef156418fd058e9852d1a0180e4536eec
Tony Duan <tony84727@gmail.com>
2023-04-02 20:58:19 +0800

    feat(leetcode/2300): brute force

---
+++ src/leetcode/algorithm_2300/README.md
@@ -0,0 +1,3 @@
+Successful Pairs of Spells and Potions
+======================================
+[leetcode](https://leetcode.com/problems/successful-pairs-of-spells-and-potions)
---
+++ src/leetcode/algorithm_2300/mod.rs
@@ -0,0 +1,12 @@
+pub mod brute;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(vec![5,1,3], vec![1,2,3,4,5],7 => vec![4,0,3]; "example 1")]
+    fn test_brute_solution(spells: Vec<i32>, potions: Vec<i32>, success: i64) -> Vec<i32> {
+        brute::Solution::successful_pairs(spells, potions, success)
+    }
+}
