076e94115848c35da7325414ff267c114aae732f
Tony Duan <tony84727@gmail.com>
2023-04-02 21:09:27 +0800

    feat(leetcode/2300): sort

--- src/leetcode/algorithm_2300/mod.rs
+++ src/leetcode/algorithm_2300/mod.rs
@@ -1,4 +1,5 @@
 pub mod brute;
+pub mod sort;
 
 #[cfg(test)]
 mod tests {
@@ -8,5 +9,9 @@ mod tests {
     #[test_case(vec![5,1,3], vec![1,2,3,4,5],7 => vec![4,0,3]; "example 1")]
     fn test_brute_solution(spells: Vec<i32>, potions: Vec<i32>, success: i64) -> Vec<i32> {
         brute::Solution::successful_pairs(spells, potions, success)
+    }
+    #[test_case(vec![5,1,3], vec![1,2,3,4,5],7 => vec![4,0,3]; "example 1")]
+    fn test_sort_solution(spells: Vec<i32>, potions: Vec<i32>, success: i64) -> Vec<i32> {
+        sort::Solution::successful_pairs(spells, potions, success)
     }
 }
