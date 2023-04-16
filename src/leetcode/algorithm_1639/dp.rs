a88d2116fa0dce73f7764d5497e7994c8a5df4bf
Tony Duan <tony84727@gmail.com>
2023-04-16 21:10:46 +0800

    feat(leetcode/1639): dp
    editorial solution

--- src/leetcode/algorithm_1639/dp.rs
+++ src/leetcode/algorithm_1639/dp.rs
@@ -1,55 +1,36 @@
-use std::collections::HashMap;
-
 pub struct Solution;
 
 impl Solution {
     pub fn num_ways(words: Vec<String>, target: String) -> i32 {
         let chars = target.chars().collect::<Vec<char>>();
         let dictionary_length = words[0].len();
-        let words = Self::index_chars(words);
-        let mut dp = vec![vec![None; chars.len()]; dictionary_length + 1];
-        Self::ways(&mut dp, &words, &chars, 0, 0)
-    }
-
-    fn index_chars(words: Vec<String>) -> Vec<HashMap<char, Vec<usize>>> {
-        words
-            .into_iter()
-            .map(|s| {
-                let mut index = HashMap::<char, Vec<usize>>::new();
-                for (i, c) in s.char_indices() {
-                    index.entry(c).or_default().push(i);
+        let occurences = Self::chars_occurrences(words);
+        let m = 1000000007;
+        let mut dp = vec![vec![0; dictionary_length + 1]; chars.len() + 1];
+        dp[0][0] = 1;
+        for i in 0..target.len() + 1 {
+            for j in 0..dictionary_length {
+                if i < chars.len() {
+                    let head = chars[i];
+                    dp[i + 1][j + 1] += ((dp[i][j] as i64
+                        * occurences[(head as u8 - 'a' as u8) as usize][j] as i64)
+                        % m as i64) as i32;
+                    dp[i + 1][j + 1] %= m;
                 }
-                index
-            })
-            .collect()
-    }
-
-    fn ways(
-        dp: &mut Vec<Vec<Option<i32>>>,
-        words: &Vec<HashMap<char, Vec<usize>>>,
-        chars: &[char],
-        i: usize,
-        start: usize,
-    ) -> i32 {
-        if start >= chars.len() {
-            return 1;
-        }
-        if let Some(answer) = dp[i][start] {
-            return answer;
-        }
-        let head = chars[start];
-        let mut ways = 0_i64;
-        for w in words.iter() {
-            if let Some(indexes) = w.get(&head) {
-                for char_index in indexes.iter() {
-                    if *char_index >= i {
-                        ways += Self::ways(dp, words, chars, char_index + 1, start + 1) as i64;
-                    }
-                }
+                dp[i][j + 1] += dp[i][j];
+                dp[i][j + 1] %= m;
             }
         }
-        let ways = (ways % 1000000007) as i32;
-        dp[i][start] = Some(ways);
-        ways
+        dp[target.len()][dictionary_length]
+    }
+
+    fn chars_occurrences(words: Vec<String>) -> Vec<Vec<i32>> {
+        let mut occurrences = vec![vec![0; words[0].len()]; 26];
+        for w in words.into_iter() {
+            for (i, c) in w.char_indices() {
+                occurrences[((c as u8) - ('a' as u8)) as usize][i] += 1;
+            }
+        }
+        occurrences
     }
 }
