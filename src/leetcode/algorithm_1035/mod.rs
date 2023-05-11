c913a0308fcae0f9c0ebc70bad690579314952ab
Tony Duan <tony84727@gmail.com>
2023-05-11 21:26:47 +0800

    leetcode(1035): cache

--- src/leetcode/algorithm_1035/mod.rs
+++ src/leetcode/algorithm_1035/mod.rs
@@ -1,3 +1,4 @@
+pub mod cache;
 pub mod recursive;
 
 #[cfg(test)]
@@ -8,5 +9,10 @@ mod tests {
     #[test_case(vec![1,4,2], vec![1,2,4] => 2; "example 1")]
     fn test_recursive(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
         recursive::Solution::max_uncrossed_lines(nums1, nums2)
+    }
+
+    #[test_case(vec![1,4,2], vec![1,2,4] => 2; "example 1")]
+    fn test_cache(nums1: Vec<i32>, nums2: Vec<i32>) -> i32 {
+        cache::Solution::max_uncrossed_lines(nums1, nums2)
     }
 }
