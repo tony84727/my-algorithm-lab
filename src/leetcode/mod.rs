601a1d08f70a9a218e1ce3dea0a47c3592ed5391
Tony Duan <tony84727@gmail.com>
2023-05-30 20:39:47 +0800

    leetcode/705: cheat

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -97,6 +97,7 @@ pub mod algorithm_652;
 pub mod algorithm_668;
 pub mod algorithm_67;
 pub mod algorithm_704;
+pub mod algorithm_705;
 pub mod algorithm_72;
 pub mod algorithm_721;
 pub mod algorithm_73;
