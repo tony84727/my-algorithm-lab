b0911bafb3037a513988b623118dde9b6122af7d
Tony Duan <tony84727@gmail.com>
2025-06-04 21:38:13 +0800

    fix #122: refine completeness checks

--- src/leetcode/algorithm_958/mod.rs
+++ src/leetcode/algorithm_958/mod.rs
@@ -39,8 +39,6 @@ mod tests {
     use crate::leetcode::common::TreeNode;
     use test_case::test_case;
 
-    #[test_case(vec!["1", "2", "3", "4", "5", "6"] => true; "example 1")]
-    #[test_case(vec!["1", "2", "3", "4", "5", "null", "7"] => false; "example 2")]
     #[test_case(vec!["1", "2", "3", "4", "5", "6", "7"] => true; "full tree")]
     #[test_case(vec!["1", "2", "3", "4", "null", "6", "7"] => false; "missing right before left")]
     fn test_solution(preorder: Vec<&str>) -> bool {
