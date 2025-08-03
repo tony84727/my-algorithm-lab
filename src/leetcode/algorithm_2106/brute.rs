06a4d106a38b19d6bcd2416feee34e2432697793
Tony Duan <tony84727@gmail.com>
2025-08-03 11:36:00 +0800

    leetcode/2106: optimize brute by ordered map

--- src/leetcode/algorithm_2106/brute.rs
+++ src/leetcode/algorithm_2106/brute.rs
@@ -1,41 +1,37 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 pub struct Solution;
 
 impl Solution {
     pub fn max_total_fruits(fruits: Vec<Vec<i32>>, start_pos: i32, k: i32) -> i32 {
-        let mut fruit_map = HashMap::new();
+        let mut fruit_map = BTreeMap::new();
         for f in fruits.into_iter() {
             fruit_map.insert(f[0], f[1]);
         }
-        Self::move_to(&mut fruit_map, start_pos + 1, k - 1).max(Self::move_to(
-            &mut fruit_map,
-            start_pos - 1,
-            k - 1,
-        ))
+        Self::move_to(&mut fruit_map, start_pos, k)
     }
 
-    fn move_to(fruits: &mut HashMap<i32, i32>, start_pos: i32, k: i32) -> i32 {
+    fn move_to(fruits: &mut BTreeMap<i32, i32>, start_pos: i32, k: i32) -> i32 {
         if k < 0 || start_pos < 0 {
             return 0;
         }
         let current = fruits.remove(&start_pos);
+        let next_left = fruits.range(..start_pos).next_back().map(|(k, _v)| *k);
+        let next_right = fruits.range(start_pos..).next().map(|(k, _v)| *k);
+        let move_left = next_left
+            .map(|left| Self::move_to(fruits, left, k - (start_pos - left)))
+            .unwrap_or_default();
+        let move_right = next_right
+            .map(|right| Self::move_to(fruits, right, k - (right - start_pos)))
+            .unwrap_or_default();
+        let next_max = move_left.max(move_right);
         match current {
             Some(current) => {
-                let harvested = current
-                    + Self::move_to(fruits, start_pos - 1, k - 1).max(Self::move_to(
-                        fruits,
-                        start_pos + 1,
-                        k - 1,
-                    ));
+                let harvested = current + next_max;
                 fruits.insert(start_pos, current);
                 harvested
             }
-            None => Self::move_to(fruits, start_pos - 1, k - 1).max(Self::move_to(
-                fruits,
-                start_pos + 1,
-                k - 1,
-            )),
+            None => next_max,
         }
     }
 }
