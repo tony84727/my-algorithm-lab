fe8958141f641dcd51d5f1193d609fc37685ba20
Tony Duan <tony84727@gmail.com>
2025-09-02 16:35:12 +0800

    leetcode/3025: brute

--- src/leetcode/algorithm_3025/mod.rs
+++ src/leetcode/algorithm_3025/mod.rs
@@ -1,32 +1,32 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn number_of_pairs(mut points: Vec<Vec<i32>>) -> i32 {
-        let mut anchor_x = i32::MAX;
-        let mut anchor_y = 0;
-        for p in points.iter() {
-            anchor_x = anchor_x.min(p[0]);
-            anchor_y = anchor_y.max(p[1]);
-        }
-        points.sort_unstable_by_key(|a| (a[0] - anchor_x).pow(2) + (a[1] - anchor_y).pow(2));
+    pub fn number_of_pairs(points: Vec<Vec<i32>>) -> i32 {
         let mut count = 0;
-        let mut x = anchor_x;
-        let mut y = anchor_y;
-        let first_point = points.first().unwrap();
-        let anchor_exist = first_point[0] == x && first_point[1] == y;
-        for p in points.iter().skip(1) {
-            if p[0] < x || p[1] > y {
-                continue;
+        for (i, a) in points.iter().enumerate() {
+            'search: for (j, b) in points.iter().enumerate() {
+                if i == j {
+                    continue;
+                }
+                if a[0] > b[0] || a[1] < b[1] {
+                    continue;
+                }
+                for (c, inside) in points.iter().enumerate() {
+                    if c == i || c == j {
+                        continue;
+                    }
+                    if inside[0] >= a[0]
+                        && inside[0] <= b[0]
+                        && inside[1] >= b[1]
+                        && inside[1] <= a[1]
+                    {
+                        continue 'search;
+                    }
+                }
+                count += 1;
             }
-            x = p[0];
-            y = p[1];
-            count += 1;
         }
-        if anchor_exist {
-            count
-        } else {
-            count - 1
-        }
+        count
     }
 }
 
