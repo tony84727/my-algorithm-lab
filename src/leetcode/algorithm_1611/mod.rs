7e5f278cac45a8c27b3950178523bb1f8f55e2c0
Tony Duan <tony84727@gmail.com>
2025-11-08 16:49:46 +0800

    leetcode/1611: editorial solution

---
+++ src/leetcode/algorithm_1611/mod.rs
@@ -0,0 +1,26 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn minimum_one_bit_operations(n: i32) -> i32 {
+        if n == 0 {
+            return 0;
+        }
+        let mut power = 0;
+        while (1 << power + 1) <= n {
+            power += 1;
+        }
+        (1 << power + 1) - 1 - Self::minimum_one_bit_operations((1 << power) ^ n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(3 => 2; "example 1")]
+    #[test_case(6 => 4; "example 2")]
+    fn test_solution(n: i32) -> i32 {
+        Solution::minimum_one_bit_operations(n)
+    }
+}
