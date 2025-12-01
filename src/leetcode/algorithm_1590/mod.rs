6c290637fe99a92a94090962279193bcae301fe0
Tony Duan <tony84727@gmail.com>
2025-12-01 22:04:29 +0800

    fix clippy suggestions

--- src/leetcode/algorithm_1590/mod.rs
+++ src/leetcode/algorithm_1590/mod.rs
@@ -16,8 +16,8 @@ impl Solution {
         map.insert(0, -1);
         let mut min_len = n as i32;
 
-        for i in 0..n {
-            prefix_rem = (prefix_rem + nums[i] as i64) % p as i64;
+        for (i, &num) in nums.iter().enumerate() {
+            prefix_rem = (prefix_rem + num as i64) % p as i64;
             let complement_rem = (prefix_rem - target_rem + p as i64) % p as i64;
             if let Some(&j) = map.get(&complement_rem) {
                 min_len = min_len.min(i as i32 - j);
